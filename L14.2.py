86b438caf9885dbf34ddcac97cadc08f82e23ad8
wYeha <90753559+wYeha@users.noreply.github.com>
2022-12-09 22:09:17 +0300

    Update L14.2.py

--- L14.2.py
+++ L14.2.py
@@ -1,32 +1,55 @@
+import sys
 import tkinter
 import random
+import os
 
 d={'Птица':'bird', 'Кот':'cat', 'Пес':'dog', 'Машина':'car', 'Голова':'head'}
 window = tkinter.Tk()
 window.geometry('200x300')
 frame = tkinter.Frame(window)
-frame.grid()
+frame.pack()
 label = tkinter.Label(frame)
-label.grid(row=0, column=0)
-entry = tkinter.Entry(frame)
-entry.grid(row=0,column=2)
-label2=tkinter.Label(frame, text="на английском")
-label2.grid(row=0, column=1)
-label3=tkinter.Label(frame)
-label3.grid(row=0, column=3, text=" ")
+answer = tkinter.Label(frame)
+label.pack()
+entry = tkinter.Entry(frame, width=10)
+entry.pack()
+answer.pack()
+
 
 def random_word():
-    word=random.choice(list(d.keys()))
+    word = random.choice(list(d.keys()))
     label.configure(text=word)
+    answer.configure(text="")
+    entry.delete(0, tkinter.END)
     return word
+
+
 random_word()
 
-def recognize():
-    l=entry.get()
-    if d[label.get()]==l :
-        label3.configure(text="да")
-    else:
-        label3.configure(text="нет")
-recognize()
 
+def restart():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+
+def recognize():
+    a = entry.get()
+    if d[label.cget('text')] == a:
+        result = 'Да'
+    else:
+        result = 'Нет'
+        recognize.invocations -=1
+        if recognize.invocations == 0:
+            result = 'Проиграли, играем заново'
+            restart()
+    answer.configure(text=result)
+
+
+recognize.invocations = 3
+
+
+btn1 = tkinter.Button(frame, text="Проверка", command=recognize)
+btn1.pack()
+btn2 = tkinter.Button(frame, text="Следующее слово", command=random_word)
+btn2.pack()
 window.mainloop()
